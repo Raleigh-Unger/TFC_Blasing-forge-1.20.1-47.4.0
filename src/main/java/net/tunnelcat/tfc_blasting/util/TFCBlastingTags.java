e516b65c353c9e708ba6a81c4dc8f1a49719c858
Raleigh-Unger <raleigh.u@outlook.com>
2025-06-22 20:02:46 -0500

    Add tags for using Artisanal drums

--- src/main/java/net/tunnelcat/tfc_blasting/util/TFCBlastingTags.java
+++ src/main/java/net/tunnelcat/tfc_blasting/util/TFCBlastingTags.java
@@ -1,9 +1,11 @@
 package net.tunnelcat.tfc_blasting.util;
 
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
 import net.tunnelcat.tfc_blasting.TFCBlasting;
 
@@ -14,10 +16,18 @@ public class TFCBlastingTags {
         public static final TagKey<Item> FUSE_CAPS = itemTag("fuse_caps");
         public static final TagKey<Item> FUSE_CAPS_EMPTY = itemTag("fuse_caps_empty");
         public static final TagKey<Item> STAR_DRILLS = itemTag("star_drills");
+
+        public static final TagKey<Block> CAN_STAR_DRILL = blockTag("can_star_drill");
+
         public static final TagKey<Fluid> ACIDS = fluidTag("acids");
+        public static final TagKey<Fluid> FLUIDS = fluidTag("fluids");
 
         private static TagKey<Item> itemTag(String name) {
             return ItemTags.create(fromNamespaceAndPath(TFCBlasting.MOD_ID, name));
+        }
+
+        private static TagKey<Block> blockTag(String name) {
+            return BlockTags.create(fromNamespaceAndPath(TFCBlasting.MOD_ID, name));
         }
 
         private static TagKey<Fluid> fluidTag(String name) {
